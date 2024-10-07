65cd4a0abcfe51754df3d71f93c094f1ae463d2c
dee <178827694+dee797@users.noreply.github.com>
2024-10-06 19:35:58 -0600

    Functionality for saving/reading from localStorage

--- src/index.js
+++ src/index.js
@@ -1,4 +1,42 @@
-import { addEvents } from "./dom";
+import { Project, Todo, projects, setItems } from "./classes";
+import { addEvents, toggleProjectDisplay, toggleTaskDisplay } from "./dom";
+import { appendProjects, appendTask } from "./app";
 import "./styles.css";
 
+
+
+const getItems = () => {
+    return JSON.parse(localStorage.getItem("projects"));
+}
+
+
+addEventListener("DOMContentLoaded", () => {
+    const items = getItems();
+    
+    if (items !== null && items.length !== 0) {
+
+        for (const item of items) {
+            const newProject = new Project(item.projectName, item.projectID);
+            const newProjectList = newProject.list;
+            projects.push(newProject);
+
+
+            for (const task of item.list) {
+                const newTask = new Todo(task.title, task.description, task.dueDate,
+                                task.priority, task.projectID, task.isDone, task.container);
+                newProjectList.push(newTask);
+                appendTask(newTask);
+            }
+        }
+        console.log(projects);
+        appendProjects();
+        toggleTaskDisplay();
+        toggleProjectDisplay();
+    }
+});
+
 addEvents();
+
+addEventListener("beforeunload", () =>  {
+    if (getItems()) setItems();
+});
